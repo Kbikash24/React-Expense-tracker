ddef7efa585f46eb3b38e6d5c5ec594f5c8fd21d
Kbikash24 <bikashk11@gmail.com>
2023-07-16 00:42:08 +0530

    filter_updt

--- task_9/src/components/Expenses/ExpensesList.js
+++ task_9/src/components/Expenses/ExpensesList.js
@@ -5,9 +5,27 @@ import './ExpensesList.css';
 
 const ExpensesList = (props) => {
   if (props.items.length === 0) {
-    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+    return <h3 className='expenses-list__fallback'>No Expenses found</h3>;
   }
-
+ 
+  if (props.items.length === 1) {
+    return (
+      <div>
+       <ul className='expenses-list'>
+          {props.items.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+        </ul>
+        <h3 className='expenses-list__fallback'>Only single Expense here. Please add more...</h3>
+      </div>
+    );
+  }
+ 
   return (
     <ul className='expenses-list'>
       {props.items.map((expense) => (
